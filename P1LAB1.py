685cb840f63192e6c67be7e5d8a3b92aa7724f54
WL9995 <warnerl9995@student.faytechcc.edu>
2026-02-20 13:27:26 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+#Lacey Warner
+#02Feb2026
+#P1LAB1
+#This is a code for entering in your first and last name and then it will print a welcome message with your name.
+
+
+
+
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+print("Hello, " + firstname + " " + lastname + "! " + "Welcome to CTI-110!")
